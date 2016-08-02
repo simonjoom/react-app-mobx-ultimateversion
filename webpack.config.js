348a291e1476a40298229bd506d22ff1ec769203
simonjoom <simon.gendrin@free.fr>
2016-08-03 01:11:53 +0200

    end commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
-require('dotenv').config();
-const cssnano = require('cssnano')
-const extend = require('extend')
+/**
+ * React App SDK (https://github.com/kriasoft/react-app)
+ *
+ * Copyright © 2015-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+/* eslint-disable global-require */
+
 const path = require('path');
 const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const AssetsPlugin = require('assets-webpack-plugin');
 const pkg = require(path.resolve(process.cwd(), './package.json'));
 
@@ -16,101 +23,91 @@ const babelConfig = Object.assign({}, pkg.babel, {
   cacheDirectory: hmr,
 });
 
-const AUTOPREFIXER_BROWSERS = [
-    'Android >= 4',
-    'Chrome >= 35',
-    'Firefox >= 34',
-    'Explorer >= 9',
-    'iOS >= 7',
-    'Opera >= 12',
-    'Safari >= 7.1',
-];
-const exposevar = {
-    'process': {
-        'env': {
-            'BROWSER': true,
-            'WEB_HOST': JSON.stringify(process.env['WEB_HOST']),
-            'WEB_PORT': JSON.stringify(process.env['WEB_PORT']),
-            'API_HOST': JSON.stringify(process.env['API_HOST']),
-            'API_PORT': JSON.stringify(process.env['API_PORT']),
-            'IO_HOST': JSON.stringify(process.env['IO_HOST']),
-            'IO_PORT': JSON.stringify(process.env['IO_PORT'])
-        }
-    }
-}
-console.log(`babel-loader?${JSON.stringify(babelConfig)}`)
-
-let BASE_CSS_LOADER;
-if (!debug)
- BASE_CSS_LOADER = 'css?-minimize'
-else
-BASE_CSS_LOADER = 'css?sourceMap&-minimize'
-
-// Add any packge names here whose styles need to be treated as CSS modules.
-// These paths will be combined into a single regex.
-const PATHS_TO_TREAT_AS_CSS_MODULES = [
-  // 'react-toolbox', (example)
-]
-
-// If config has CSS modules enabled, treat this project's styles as CSS modules.
-
-  PATHS_TO_TREAT_AS_CSS_MODULES.push(
-    process.cwd().replace(/[\^\$\.\*\+\-\?\=\!\:\|\\\/\(\)\[\]\{\}\,]/g, '\\$&') // eslint-disable-line
-  )
-const localIdentName= debug ? '[name]_[local]_[hash:base64:3]' : '[hash:base64:4]';
-  const cssModulesLoader = [
-    BASE_CSS_LOADER,
-    'modules',
-    'importLoaders=1',
-    'localIdentName='+localIdentName
-   // 'localIdentName=[name]__[local]___[hash:base64:5]'
-  ].join('&')
-const isUsingCSSModules = !!PATHS_TO_TREAT_AS_CSS_MODULES.length
-const cssModulesRegex = new RegExp(`(${PATHS_TO_TREAT_AS_CSS_MODULES.join('|')})`)
-
-
-const configvar={
-      'process.env.NODE_ENV': debug ? '"development"' : '"production"',
-      __DEV__: debug,
-    }
 // Webpack configuration (main.js => public/dist/main.{hash}.js)
 // http://webpack.github.io/docs/configuration.html
-const webpackConfig = {
-  plugins: [],
+const config = {
+
+  // The base directory for resolving the entry option
   context: process.cwd(),
-    // Switch loaders to debug or release mode
-  debug: debug,
+
+  // The entry point for the bundle
+  entry: [
+    /* The main entry point of your JavaScript application */
+    './main.js',
+  ],
+
+  // Options affecting the output of the compilation
+  output: {
+    path: path.resolve(process.cwd(), './public/dist'),
+    publicPath: '/dist/',
+    filename: debug ? '[name].js?[hash]' : '[name].[hash].js',
+    chunkFilename: debug ? '[id].js?[chunkhash]' : '[id].[chunkhash].js',
+    sourcePrefix: '  ',
+  },
+
+  // Switch loaders to debug or release mode
+  debug,
+
   // Developer tool to enhance debugging, source maps
   // http://webpack.github.io/docs/configuration.html#devtool
   devtool: debug ? 'source-map' : false,
 
-    resolve: {
-    },
-    cache: false,
+  // What information should be printed to the console
+  stats: {
+    colors: true,
+    reasons: debug,
+    hash: verbose,
+    version: verbose,
+    timings: true,
+    chunks: verbose,
+    chunkModules: verbose,
+    cached: verbose,
+    cachedAssets: verbose,
+  },
+
+  // The list of plugins for Webpack compiler
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': debug ? '"development"' : '"production"',
+      __DEV__: debug,
+    }),
+    // Emit a JSON file with assets paths
+    // https://github.com/sporto/assets-webpack-plugin#options
+    new AssetsPlugin({
+      path: path.resolve(process.cwd(), './public/dist'),
+      filename: 'assets.json',
+      prettyPrint: true,
+    }),
+  ],
 
   // Options affecting the normal modules
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
         include: [
           path.resolve(process.cwd(), './actions'),
           path.resolve(process.cwd(), './components'),
           path.resolve(process.cwd(), './core'),
           path.resolve(process.cwd(), './pages'),
           path.resolve(process.cwd(), './routes'),
-          path.resolve(process.cwd(), ''),
           path.resolve(process.cwd(), './main.js'),
         ],
-       loader: `babel-loader?${JSON.stringify(babelConfig)}`,
+        loader: `babel-loader?${JSON.stringify(babelConfig)}`,
       },
       {
         test: /\.css/,
-                  include: cssModulesRegex,
         loaders: [
-          'simple-universal-style',
-          cssModulesLoader,
+          'style-loader',
+          `css-loader?${JSON.stringify({
+            sourceMap: debug,
+            // CSS Modules https://github.com/css-modules/css-modules
+            modules: true,
+            localIdentName: debug ? '[name]_[local]_[hash:base64:3]' : '[hash:base64:4]',
+            // CSS Nano http://cssnano.co/options/
+            minimize: !debug,
+          })}`,
           'postcss-loader',
         ],
       },
@@ -127,8 +124,7 @@ const webpackConfig = {
           path.resolve(process.cwd(), './routes.json'),
         ],
         loaders: [
-         //'babel',
-           `babel-loader?${JSON.stringify(babelConfig)}`,
+          `babel-loader?${JSON.stringify(babelConfig)}`,
           path.resolve(process.cwd(), './utils/routes-loader.js'),
         ],
       },
@@ -189,78 +185,16 @@ const webpackConfig = {
       require('postcss-selector-not')(),
       // Add vendor prefixes to CSS rules using values from caniuse.com
       // https://github.com/postcss/autoprefixer
-      cssnano({
-    autoprefixer: {
-      add: true,
-      remove: true,
-      browsers: ['last 2 versions']
-    },
-    discardComments: {
-      removeAll: true
-    },
-    discardUnused: false,
-    mergeIdents: false,
-    reduceIdents: false,
-    safe: true,
-    sourcemap: true
-  })
+      require('autoprefixer')(),
     ];
   },
 
-}
-
-
-const clientConfig = extend(true, {}, webpackConfig, {
-
-  // The entry point for the bundle
-  entry: [
-    /* The main entry point of your JavaScript application */
-    './main.js',
-  ],
-
-  // Options affecting the output of the compilation
-  output: {
-    path: path.resolve(process.cwd(), './public/dist'),
-    publicPath: '/dist/',
-    filename: debug ? '[name].js?[hash]' : '[name].[hash].js',
-   // chunkFilename: debug ? '[id].js?[chunkhash]' : '[id].[chunkhash].js',
-    sourcePrefix: '  ',
-  },
-
-  // What information should be printed to the console
-  stats: {
-    colors: true,
-    reasons: debug,
-    hash: verbose,
-    version: verbose,
-    timings: true,
-    chunks: verbose,
-    chunkModules: verbose,
-    cached: verbose,
-    cachedAssets: verbose,
-  },
-
-  // The list of plugins for Webpack compiler
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-
-    new webpack.DefinePlugin(extend(true, {}, configvar, exposevar)),
-    new CopyWebpackPlugin([{from: 'assets', to: ''}]),
-    // Emit a JSON file with assets paths
-    // https://github.com/sporto/assets-webpack-plugin#options
-    new AssetsPlugin({
-      path: path.resolve(process.cwd(), './public/dist'),
-      filename: 'assets.json',
-      prettyPrint: true,
-    }),
-  ],
-
-});
+};
 
 // Optimize the bundle in release (production) mode
 if (!debug) {
-  clientConfig.plugins.push(new webpack.optimize.DedupePlugin());
-  clientConfig.plugins.push(new webpack.optimize.UglifyJsPlugin({
+  config.plugins.push(new webpack.optimize.DedupePlugin());
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
     compress: {
       screw_ie8: true,
       warnings: verbose,
@@ -273,65 +207,15 @@ if (!debug) {
       screw_ie8: true,
     },
   }));
-  clientConfig.plugins.push(new webpack.optimize.AggressiveMergingPlugin());
+  config.plugins.push(new webpack.optimize.AggressiveMergingPlugin());
 }
 
 // Hot Module Replacement (HMR) + React Hot Reload
 if (hmr) {
   babelConfig.plugins.unshift('react-hot-loader/babel');
-  clientConfig.entry.unshift('react-hot-loader/patch', 'webpack-hot-middleware/client');
-  clientConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
-  clientConfig.plugins.push(new webpack.NoErrorsPlugin());
+  config.entry.unshift('react-hot-loader/patch', 'webpack-hot-middleware/client');
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  config.plugins.push(new webpack.NoErrorsPlugin());
 }
 
-
-exposevar.process.env.BROWSER = false;
-const serverConfig = extend(true, {}, webpackConfig, {
-
-    output: {
-    path: path.resolve(process.cwd(), './public/dist'),
-        filename: 'server.js',
-        libraryTarget: 'commonjs2',
-        //path: path.resolve(__dirname, '../dist/assets')
-    },
-  entry: [
-    /* The main entry point of your JavaScript application */
-    './server.js',
-  ],
-    target: 'node',
-
-    externals: [
-        /^\.\/assets$/,
-        /ajv/,
-        function filter(context, request, cb) {
-            const isExternal =
-                request.match(/^[@a-z][a-z\/\.\-0-9]*$/i) && !request.match(/^react-routing/) && !context.match(/[\\/]react-routing/);
-            cb(null, Boolean(isExternal));
-        },
-    ],
-
-    plugins: [
-    new webpack.ProvidePlugin({
-            "React": "react"
-        }),
-        //  new HappyPack({ id: 'js' }),
-        //  new HappyPack({ id: 'css' }),
-        // Define free variables
-        // https://webpack.github.io/docs/list-of-plugins.html#defineplugin
-
-        new webpack.DefinePlugin(extend(true, {}, configvar, exposevar)),
-    ],
-
-    node: {
-        console: false,
-        global: false,
-        process: false,
-        Buffer: false,
-        __filename: false,
-        __dirname: false,
-    },
-
-});
-
-module.exports = [clientConfig, serverConfig];
-
+module.exports = config;
