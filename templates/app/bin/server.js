f676244cd0f84ab2ce7d2dfbe7ef63f443638a9c
simonjoom <simon.gendrin@free.fr>
2016-08-04 22:07:59 +0200

    end commit

--- templates/app/bin/server.js
+++ templates/app/bin/server.js
@@ -1,11 +1,11 @@
-import config from '../src/config'
-import server from '../server/main'
-import _debug from 'debug'
-
-const debug = _debug('app:bin:server')
-const port = config.server_port
-const host = config.server_host
-
-//server.listen(port)
-debug(`Server is now running at http://${host}:${port}.`)
-debug(`Server accessible via localhost:${port} if you are using the project defaults.`)
+#!/usr/bin/env node
+if (process.env.NODE_ENV !== 'production') {
+  if (!require('piping')({
+    hook: true,
+    ignore: /(\/\.|~$|\.json$|\.scss)/i
+  })) {
+    return;
+  }
+}
+require('../server.babel'); // babel registration (runtime transpilation for node)
+require('../server');
