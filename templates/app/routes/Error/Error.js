eb31d73f395ca52a94ca09085cfbee3d5908f8ea
simonjoom <simon.gendrin@free.fr>
2016-08-26 08:23:12 +0200

    end commit

--- templates/app/routes/Error/Error.js
+++ templates/app/routes/Error/Error.js
@@ -1,5 +1,6 @@
 
 import history from '../../core/history';
+import Layout from '../../components/Layout';
 import '../../components/Layout/Layout.css';
 import Link from '../../components/Link';
 import s from './Error.css';
@@ -21,15 +22,14 @@ class ErrorPage extends React.Component {
   };
 
   render() {
-    if (this.props.error) console.error(this.props.error); // eslint-disable-line no-console
-
+    //if (this.props.error) console.error(this.props.error); // eslint-disable-line no-console
+    if ((this.props.error)&&(process.env.NODE_ENV !== 'production'))
+    console.error(this.props.error.stack)
     const [code, title] = this.props.error && this.props.error.status === 404 ?
       ['404', 'Page not found'] :
       ['Error', 'Oups, something went wrong'];
-
     return (
-      <div className={s.container}>
-        <main className={s.content}>
+      <Layout>
           <h1 className={s.code}>{code}</h1>
           <p className={s.title}>{title}</p>
           {code === '404' &&
@@ -41,8 +41,7 @@ class ErrorPage extends React.Component {
             <a href="/" onClick={this.goBack}>Go back</a>, or head over to the&nbsp;
             <Link to="Home">home page</Link> to choose a new direction.
           </p>
-        </main>
-      </div>
+      </Layout>
     );
   }
 
