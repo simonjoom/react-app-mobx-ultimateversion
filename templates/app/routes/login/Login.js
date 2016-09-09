a03b3910d3f252ed3b5eefa3fe11a7d19d3709e8
simonjoom <simon.gendrin@free.fr>
2016-09-09 21:39:20 +0200

    disable COMMON CHUNK dynamic load for developpement / fix hot reload

--- templates/app/routes/login/Login.js
+++ templates/app/routes/login/Login.js
@@ -1,38 +1,39 @@
-import { PropTypes, Component } from 'react';
-import { observer } from 'mobx-react';
+import {PropTypes, Component} from 'react';
+import {observer} from 'mobx-react';
 import Login from '../../components/Login';
+
 
 @observer(['appstate'])
 class LoginPage extends Component {
-  static propTypes = {
-    appstate: PropTypes.object.isRequired,
-    lang: PropTypes.string,
-    title: PropTypes.string,
-    description: PropTypes.string,
-  };
-  static contextTypes = {
-    setTitle: PropTypes.func.isRequired,
-    setMeta: PropTypes.func,
-    muiTheme: PropTypes.object.isRequired,
-  };
-  goBack = event => {
-    event.preventDefault();
-    history.goBack();
-  };
+    static propTypes = {
+        appstate: PropTypes.object.isRequired,
+        lang: PropTypes.string,
+        title: PropTypes.string,
+        description: PropTypes.string,
+    };
+    static contextTypes = {
+        setTitle: PropTypes.func.isRequired,
+        setMeta: PropTypes.func,
+        muiTheme: PropTypes.object.isRequired,
+    };
+    goBack = event => {
+        event.preventDefault();
+        history.goBack();
+    };
 
-  render() {
+    render() {
 // eslint-disable-next-line no-unused-vars
-    const { appstate, lang, description, title } = this.props;
-    this.context.setTitle(title);
-    this.context.setMeta('description', description);
-    return (
-        <Login />
-
-        <a href="/" onClick={this.goBack}>Go back</a>, or head over to the&nbsp;
-        <a href="/">home page</a>
-
-    );
-  }
+        const {appstate, lang, description, title} = this.props;
+        this.context.setTitle(title);
+        this.context.setMeta('description', description);
+        return (
+            <div>
+                <Login />
+                <a href="/" onClick={this.goBack}>Go back</a>, or head over to the&nbsp;
+                <a href="/">home page</a>
+            </div>
+        );
+    }
 
 }
 
